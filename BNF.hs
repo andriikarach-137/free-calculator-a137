e3054327eaee7018340154776789d730801fb4a1
Andrii Karach <andrii.karach.13@gmail.com>
2026-08-08 21:00:14 +0100

    Create second version of BNF, add data Token

--- BNF.hs
+++ BNF.hs
@@ -1,18 +1,51 @@
 module BNF where 
+import Distribution.SPDX (LicenseId(EPICS))
 
 -- Backus-Naur-Form for the calculator 
 
 {-
-<code>     ::= (<stmt>)*
+RULES OF THE LANGUAGE:
+
+<program>  ::= (<stmt>)*
 <stmt>     ::= <assgn> | <expr> 
-<assgn>    ::= <var> <asgn_op> <expr>
-<expr>     ::= <term> ((<add>|<sub>) <term>)*
-<term>     ::= <factor> ((<mul>|<div>) <factor>)*
-<factor>   ::= <unary> | <unary> <pow> <factor> 
-<unary>    ::= <postfix> | <neg> <unary> | <log> <unary> | <sin> <unary> | <cos> <unary> | <tan> <unary> 
+<assgn>    ::= <var> ASSGN_OP <expr>
+<expr>     ::= <term> ((PLUS|MINUS) <term>)*
+<term>     ::= <factor> ((STAR|DASH) <factor>)*
+<factor>   ::= <unary> | <unary> <POW> <factor> 
+<unary>    ::= <postfix> | MINUS <unary> | LOG <unary> | SIN <unary> | COS <unary> | TAN <unary> 
 <postfix>  ::= primary postfix' 
-<postfix'> ::= <fact> postfix' | <eps>
-<primary>  ::= <lbr> <expr> <rbr> | <num> | <var>
-<var>      ::= <char> (<char> | <num>)*
+<postfix'> ::= FACT postfix' | EPS
+<primary>  ::= LBR <expr> RBR | NUM | IDENTIFIER | EXIT
+
+TERMINAL TOKENS:
+
+IDENTIFIER ::= CHAR (CHAR | DIGIT)*
+ASSGN_OP   ::= "<<"
+PLUS       ::= "+"
+MINUS      ::= "-"
+STAR       ::= "*"
+DASH       ::= "/"
+POW        ::= "^"
+LOG        ::= "log"
+SIN        ::= "sin"
+COS        ::= "cos"
+TAN        ::= "TAN"
+FACT       ::= "!"
+LBR        ::= "("
+RBR        ::= ")"
+EXIT       ::= ":q"
+NUM        handled internally 
+CHAR       handled internally 
+EPS        handled internally 
+
 -}
 
+data OpToken = ASSGN | FACT | LRB | RBR | PLUS | MINUS | STAR | DASH | POW 
+data FuncToken = LOG | SIN | COS | TAN 
+
+data Token 
+    = Op OpToken 
+    | Func FuncToken 
+    | NUM Double 
+    | IDENTIFIER String 
+    | EXIT 
