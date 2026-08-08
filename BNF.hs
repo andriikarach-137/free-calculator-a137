914100fe907df207644db982ae083273516b2611
Andrii Karach <andrii.karach.13@gmail.com>
2026-08-08 19:22:34 +0100

    Finalise version 1 of BNF

--- BNF.hs
+++ BNF.hs
@@ -3,13 +3,16 @@ module BNF where
 -- Backus-Naur-Form for the calculator 
 
 {-
-<assign>   ::= <var> <asgn_op> <expr>
+<code>     ::= (<stmt>)*
+<stmt>     ::= <assgn> | <expr> 
+<assgn>    ::= <var> <asgn_op> <expr>
 <expr>     ::= <term> ((<add>|<sub>) <term>)*
 <term>     ::= <factor> ((<mul>|<div>) <factor>)*
 <factor>   ::= <unary> | <unary> <pow> <factor> 
 <unary>    ::= <postfix> | <neg> <unary> | <log> <unary> | <sin> <unary> | <cos> <unary> | <tan> <unary> 
 <postfix>  ::= primary postfix' 
 <postfix'> ::= <fact> postfix' | <eps>
-<primary>  ::= <lbr> <expr> <rbr> | <val> | <var>
+<primary>  ::= <lbr> <expr> <rbr> | <num> | <var>
 <var>      ::= <char> (<char> | <num>)*
 -}
+
