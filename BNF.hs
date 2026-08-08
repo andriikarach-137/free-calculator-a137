9917032d28ab3232a69dc83fb743e6e51cb08c0d
Andrii Karach <andrii.karach.13@gmail.com>
2026-08-08 21:01:11 +0100

    Revert "Create second version of BNF, add data Token"
    This reverts commit e3054327eaee7018340154776789d730801fb4a1.

--- BNF.hs
+++ BNF.hs
@@ -1,51 +1,18 @@
 module BNF where 
-import Distribution.SPDX (LicenseId(EPICS))
 
 -- Backus-Naur-Form for the calculator 
 
 {-
-RULES OF THE LANGUAGE:
-
-<program>  ::= (<stmt>)*
+<code>     ::= (<stmt>)*
 <stmt>     ::= <assgn> | <expr> 
-<assgn>    ::= <var> ASSGN_OP <expr>
-<expr>     ::= <term> ((PLUS|MINUS) <term>)*
-<term>     ::= <factor> ((STAR|DASH) <factor>)*
-<factor>   ::= <unary> | <unary> <POW> <factor> 
-<unary>    ::= <postfix> | MINUS <unary> | LOG <unary> | SIN <unary> | COS <unary> | TAN <unary> 
+<assgn>    ::= <var> <asgn_op> <expr>
+<expr>     ::= <term> ((<add>|<sub>) <term>)*
+<term>     ::= <factor> ((<mul>|<div>) <factor>)*
+<factor>   ::= <unary> | <unary> <pow> <factor> 
+<unary>    ::= <postfix> | <neg> <unary> | <log> <unary> | <sin> <unary> | <cos> <unary> | <tan> <unary> 
 <postfix>  ::= primary postfix' 
-<postfix'> ::= FACT postfix' | EPS
-<primary>  ::= LBR <expr> RBR | NUM | IDENTIFIER | EXIT
-
-TERMINAL TOKENS:
-
-IDENTIFIER ::= CHAR (CHAR | DIGIT)*
-ASSGN_OP   ::= "<<"
-PLUS       ::= "+"
-MINUS      ::= "-"
-STAR       ::= "*"
-DASH       ::= "/"
-POW        ::= "^"
-LOG        ::= "log"
-SIN        ::= "sin"
-COS        ::= "cos"
-TAN        ::= "TAN"
-FACT       ::= "!"
-LBR        ::= "("
-RBR        ::= ")"
-EXIT       ::= ":q"
-NUM        handled internally 
-CHAR       handled internally 
-EPS        handled internally 
-
+<postfix'> ::= <fact> postfix' | <eps>
+<primary>  ::= <lbr> <expr> <rbr> | <num> | <var>
+<var>      ::= <char> (<char> | <num>)*
 -}
 
-data OpToken = ASSGN | FACT | LRB | RBR | PLUS | MINUS | STAR | DASH | POW 
-data FuncToken = LOG | SIN | COS | TAN 
-
-data Token 
-    = Op OpToken 
-    | Func FuncToken 
-    | NUM Double 
-    | IDENTIFIER String 
-    | EXIT 
